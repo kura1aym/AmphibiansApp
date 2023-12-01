82fd931369fcf6cb3e55e9d71c19db4c48efe4a5
kura1aym <mukhtarkuralay249@mail.ru>
2023-12-02 05:25:50 +0600

    add AmphibiansApp

---
+++ app/src/main/java/com/example/amphibians/ui/screens/HomeScreen.kt
@@ -0,0 +1,12 @@
+package com.example.amphibians.ui.screens
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+class HomeScreen(
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp)
+) {
+
+}
