16b802ebdfc20e3ef88e466af34b109bdb0b73db
kura1aym <mukhtarkuralay249@mail.ru>
2023-12-02 06:09:14 +0600

    add data class

--- app/src/main/java/com/example/amphibians/AmphibiansApplication.kt
+++ app/src/main/java/com/example/amphibians/AmphibiansApplication.kt
@@ -1,8 +1,13 @@
 package com.example.amphibians
 
 import android.app.Application
+import com.example.amphibians.data.AppContainer
+import com.example.amphibians.data.DefaultAppContainer
 
 class AmphibiansApplication : Application() {
+    lateinit var container: AppContainer
+    override fun onCreate() {
+        super.onCreate()
 
-    val container: Any
+    }
 }
