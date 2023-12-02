317f0b9aa995a79df8725d6650ffa0bd6b886fa2
kura1aym <mukhtarkuralay249@mail.ru>
2023-12-02 06:12:55 +0600

    add AmphibiansApiService

--- app/src/main/java/com/example/amphibians/AmphibiansApplication.kt
+++ app/src/main/java/com/example/amphibians/AmphibiansApplication.kt
@@ -8,6 +8,6 @@ class AmphibiansApplication : Application() {
     lateinit var container: AppContainer
     override fun onCreate() {
         super.onCreate()
-
+        container = DefaultAppContainer()
     }
 }
